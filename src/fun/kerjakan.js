aa471c3ed16b1d4cd41d6699e31d496a215b1b0e
bipproduction <bip.production.js@gmail.com>
2024-02-04 11:41:51 +0800

    auto push

--- src/fun/kerjakan.js
+++ src/fun/kerjakan.js
@@ -30,11 +30,11 @@ module.exports = async function ({ log = "", url = "http://localhost:3000" } = {
         log += "upload content \n"
 
 
-        // const y_up = await youtube_upload({
-        //     content
-        // })
+        const y_up = await youtube_upload({
+            content
+        })
 
-        // await fetch(`https://wa.wibudev.com/code?text=youtube success \n${y_up.join(" ")} \ncontent: ${text.text}&nom=6289697338821`)
+        await fetch(`https://wa.wibudev.com/code?text=youtube success \n${y_up.join(" ")} \ncontent: ${text.text}&nom=6289697338821`)
 
         return log
     } catch (error) {
